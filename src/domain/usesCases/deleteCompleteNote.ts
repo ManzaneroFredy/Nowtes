2bb4e110413c73d92804e9f5af1306c58b8d0782
Paola Novelo <pau.novelo0609@gmail.com>
2024-05-18 01:33:18 -0600

    feat: 💄 delete completed note

---
+++ src/domain/usesCases/deleteCompleteNote.ts
@@ -0,0 +1,34 @@
+import NoteRepository from "../repositories/NoteRepository";
+import UserNotesRepository from "../repositories/UserNotesRepository";
+
+export class DeleteNote {
+  constructor(
+    private readonly noteRepository: NoteRepository,
+    private readonly userNotesRepository: UserNotesRepository
+  ) {}
+
+  async deleteNote(id: string): Promise<void> {
+    // Lógica para eliminar la nota con el id proporcionado
+    try {
+      // Primero, obtén la nota existente
+      const existingNote = await this.userNotesRepository.findById(id);
+
+      if (!existingNote) {
+        throw new Error("No se encontró la nota para eliminar.");
+      }
+
+      // Verifica que la nota esté finalizada
+      if (existingNote.getStatus() === "Terminado") {
+        throw new Error("No se puede eliminar una nota finalizada.");
+      }
+
+      // Procede a eliminar la nota
+      await this.noteRepository.deleteNoteById(id);
+
+      console.log("¡La nota se ha eliminado exitosamente!");
+    } catch (error) {
+      console.error("Error al eliminar la nota:", error);
+      throw error;
+    }
+  }
+}
