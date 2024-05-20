95de9663459d48c3207b027c7ceb2bb0eb641b5f
Patricio P <patriciopenaojeda@gmail.com>
2024-05-19 23:27:05 -0600

    style: 🎨 Some changes at the code to meet the code standar

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,47 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "vuetify/styles";
-import { createVuetify } from "vuetify";
+import { ThemeDefinition, createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "@mdi/font/css/materialdesignicons.css";
 import router from "./router";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 import "reflect-metadata";
+
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
+
+const myCustomDarkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: "#121212",
+    surface: "#121212",
+    primary: "#ff9100",
+    "primary-darken-1": "#FFFFFF",
+    secondary: "#ff9100",
+    "secondary-darken-1": "#018786",
+    error: "#e54545",
+    info: "#ff9100",
+    success: "#6290C8",
+    warning: "#E8AAEC",
+    title: "#E3B024",
+    whiteText: "#FFFFFF",
+  },
+};
 
 const vuetify = createVuetify({
   components,
@@ -17,6 +51,18 @@ const vuetify = createVuetify({
     aliases,
     sets: {
       mdi,
+    },
+  },
+  theme: {
+    defaultTheme: "myCustomDarkTheme",
+    variations: {
+      colors: ["primary", "secondary"],
+      lighten: 1,
+      darken: 2,
+    },
+    themes: {
+      myCustomDarkTheme,
+      myCustomLightTheme,
     },
   },
 });
