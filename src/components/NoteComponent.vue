29a4daeb28133c331aa6cd644b004af0e3dc6b08
DiegoBurgos616 <A20216388@ALUMNOS.UADY.MX>
2024-05-20 20:35:33 -0600

    style: 🎨 Delete animation for pending notes

--- src/components/NoteComponent.vue
+++ src/components/NoteComponent.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-sheet rounded="xl" class="w-auto bg-nowte">
+  <v-sheet
+    v-if="!isDeleted"
+    rounded="xl"
+    class="w-auto bg-nowte"
+    :class="{ deleting: isDeleting }"
+  >
     <div class="w-50 mr-16 ml-5 mt-2">
       <h2 class="title mb-4 text-nowteText">{{ props.title }}</h2>
       <p class="title text-nowteText">
@@ -28,7 +33,9 @@
       </div>
     </div>
     <div class="d-flex flex-wrap ml-2 mt-2">
-      <div class="ml-5 w-100"><h2 class="title text-nowteText">Acción</h2></div>
+      <div class="ml-5 w-100">
+        <h2 class="title text-nowteText">Acción</h2>
+      </div>
       <div class="mt-n3">
         <v-btn
           class="mr-2"
@@ -37,7 +44,12 @@
           icon="mdi-pencil"
           @click="$emit('showEditNoteComponent', 'editNoteComponent')"
         ></v-btn>
-        <v-btn variant="text" density="comfortable" icon="mdi-delete"></v-btn>
+        <v-btn
+          variant="text"
+          density="comfortable"
+          icon="mdi-delete"
+          @click="handleDelete"
+        ></v-btn>
         <v-btn
           variant="text"
           density="comfortable"
@@ -55,7 +67,7 @@
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from "vue";
+import { defineProps, ref, getCurrentInstance } from "vue";
 
 const props = defineProps({
   title: {
@@ -75,11 +87,27 @@ const props = defineProps({
     required: true,
   },
 });
+
+const isDeleting = ref(false);
+const isDeleted = ref(false);
+const { emit } = getCurrentInstance();
+
+const handleDelete = () => {
+  isDeleting.value = true;
+  setTimeout(() => {
+    isDeleted.value = true;
+    emit("deleteNote", props.title);
+  }, 300); // Duración de la animación
+};
 </script>
 
 <style scoped lang="scss">
 .note {
-  width: 1vh;
+  transition: opacity 0.3s ease;
+}
+
+.deleting {
+  opacity: 0;
 }
 
 .title {
